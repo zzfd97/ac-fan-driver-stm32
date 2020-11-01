a582568193d86d9bcd829c6e23fd8d2b8145eed3
Grzegorz Cholewa <grzesiekcholewa@gmail.com>
2020-11-01 20:34:25 +0100

    Doxygen-style comments added to Modbus module

--- Core/Inc/modbus.c
+++ Core/Inc/modbus.c
@@ -1,31 +1,39 @@
 #include <string.h>
-#include <crc.h>
 #include <modbus.h>
-#include <rs485.h>
+#include <crc.h>
 
+/* LOCAL DEFINES */
 #define MAX_REGISTERS_OFFSET (REGISTERS_NUMBER-1)
-#define MODBUS_FUNCTION_READ 0x03
-#define MODBUS_FUNCTION_WRITE 0x06
-#define FRAME_ERROR_LENGTH -1
-#define FRAME_ERROR_CRC -2
+#define FUNCTION_READ_MULTIPLE 0x03
+#define FUNCTION_WRITE_SINGLE 0x06
+#define POSITION_DEVICE_ID 0
+#define POSITION_FUNCTION 1
 
 /* STATIC FUNCTION DECLARATIONS */
 uint8_t get_high_byte(uint16_t two_byte);
 uint8_t get_low_byte(uint16_t two_byte);
 uint16_t get_short_little_endian(uint8_t * first_byte_pointer);
 uint16_t get_short_big_endian(uint8_t * first_byte_pointer);
-void get_info_registers(modbus_register  * data, uint16_t data_length);
 void print_buffer(uint8_t * buffer, uint16_t length);
-
 
 /* GLOBAL VARIABLES */
 static int16_t registers[REGISTERS_NUMBER]; // allocates memory for modbus register data
-uint8_t write_request_head[] = {DEVICE_ID, MODBUS_FUNCTION_WRITE};
-uint8_t read_request_head[] = {DEVICE_ID, MODBUS_FUNCTION_READ};
+
+
+int16_t modbus_get_reg_value(uint16_t offset)
+{
+	if (offset > MAX_REGISTERS_OFFSET)
+	{
+		// error handling
+	}
+
+	return registers[offset];
+}
+
 
 bool modbus_set_reg_value(uint16_t offset, int16_t value)
 {
-	if (offset > REGISTERS_NUMBER -1)
+	if (offset > MAX_REGISTERS_OFFSET)
 	{
 		return false;
 	}
@@ -34,42 +42,9 @@ bool modbus_set_reg_value(uint16_t offset, int16_t value)
 	return true;
 }
 
-int16_t modbus_get_reg_value(uint16_t offset)
-{
-	if (offset > REGISTERS_NUMBER -1)
-	{
-		// error handling
-	}
-
-	return registers[offset];
-}
-
-uint8_t get_high_byte(uint16_t two_byte) 
-{
-	return ((two_byte >> 8) & 0xFF); // MSB 
-}
-
-uint8_t get_low_byte(uint16_t two_byte) 
-{
-	
-	return (two_byte & 0xFF); // LSB
-}
-
-uint16_t get_short_little_endian(uint8_t * first_byte_pointer) // first byte is low byte
-{
-	return (short) (*(first_byte_pointer+1) << 8 | *(first_byte_pointer));
-}
-
-uint16_t get_short_big_endian(uint8_t * first_byte_pointer) // first byte is high byte
-{
-	return (short) (*first_byte_pointer << 8 | *(first_byte_pointer+1));
-}
 
 bool modbus_process_frame(uint8_t * request, uint16_t request_size, uint8_t * response, uint16_t * response_size)
 {
-//	printf("Received Modbus frame: ");
-//	print_buffer(frame, frame_size);
-
 	// check CRC
 	uint16_t crc_calculated = crc16_modbus(request, request_size-2);
 	uint16_t crc_received = get_short_little_endian(request+request_size-2);
@@ -80,10 +55,10 @@ bool modbus_process_frame(uint8_t * request, uint16_t request_size, uint8_t * re
 	}
 	
 	// read request received
-	switch (request[1])
+	switch (request[POSITION_FUNCTION])
 	{
-		case MODBUS_FUNCTION_READ:
-			printf("Read request received, ");
+		case FUNCTION_READ_MULTIPLE:
+			printf("Request 0x%02x received, ", FUNCTION_READ_MULTIPLE);
 			uint16_t first_address_offset = get_short_big_endian(request+2);
 			uint16_t registers_number = get_short_big_endian(request+4);
 			printf("first register offset: %d, number of registers: %d\n", first_address_offset, registers_number);
@@ -97,8 +72,8 @@ bool modbus_process_frame(uint8_t * request, uint16_t request_size, uint8_t * re
 			*response_size = 3 + 2*registers_number + 2;
 
 			/* Add constant elements */
-			*(response + 0) = DEVICE_ID;
-			*(response + 1) = MODBUS_FUNCTION_READ;
+			*(response + POSITION_DEVICE_ID) = DEVICE_ID;
+			*(response + POSITION_FUNCTION) = FUNCTION_READ_MULTIPLE;
 			*(response + 2) = 2*registers_number;
 
 			/* Add data registers */
@@ -115,35 +90,35 @@ bool modbus_process_frame(uint8_t * request, uint16_t request_size, uint8_t * re
 
 			break;
 	
-	// write request received
-	case MODBUS_FUNCTION_WRITE:
-		printf("Write request received, ");
-		uint16_t register_offset = get_short_big_endian(request+2);
-		int16_t value_to_set = get_short_big_endian(request+4);
-		printf("setting register with offset %d, value to set: %d\n", register_offset, value_to_set);
-		
-		if (register_offset > MAX_REGISTERS_OFFSET)
-		return FRAME_ERROR_LENGTH;
-		
-		if (register_offset >= REGISTERS_NUMBER)
-		{
-			printf("ERROR: modbus_process_frame, requested registers not valid\n");
+		case FUNCTION_WRITE_SINGLE:
+			printf("Request 0x%02x received, ", FUNCTION_WRITE_SINGLE);
+			uint16_t register_offset = get_short_big_endian(request+2);
+			int16_t value_to_set = get_short_big_endian(request+4);
+			printf("setting register with offset %d, value to set: %d\n", register_offset, value_to_set);
+
+			if (register_offset > MAX_REGISTERS_OFFSET)
+			{
+				return false;
+			}
+
+			if (register_offset >= REGISTERS_NUMBER)
+			{
+				printf("ERROR: modbus_process_frame, requested registers not valid\n");
+				return false;
+			}
+
+			registers[register_offset] = value_to_set;
+			memcpy(response, request, request_size); // response for that command is echo
+			*response_size = request_size;
+			break;
+
+		default:
+			printf("ERROR: modbus_process_frame, unsupported function detected\n");
 			return false;
-		}
-		registers[register_offset] = value_to_set;
-		// response for that command is echo
-		memcpy(response, request, request_size);
-		response_size = request_size;
-		break;
 	}
 	return true;
 }
 
-
-void get_info_registers(modbus_register  * data, uint16_t data_length)
-{
-	memcpy(registers, data, data_length);
-}
 
 void print_buffer(uint8_t * buffer, uint16_t length)
 {
@@ -154,3 +129,26 @@ void print_buffer(uint8_t * buffer, uint16_t length)
 	printf("\n");
 }
 
+
+uint8_t get_high_byte(uint16_t two_byte)
+{
+	return ((two_byte >> 8) & 0xFF); // MSB
+}
+
+
+uint8_t get_low_byte(uint16_t two_byte)
+{
+	return (two_byte & 0xFF); // LSB
+}
+
+
+uint16_t get_short_little_endian(uint8_t * first_byte_pointer) // first byte is low byte
+{
+	return (short) (*(first_byte_pointer+1) << 8 | *(first_byte_pointer));
+}
+
+
+uint16_t get_short_big_endian(uint8_t * first_byte_pointer) // first byte is high byte
+{
+	return (short) (*first_byte_pointer << 8 | *(first_byte_pointer+1));
+}
--- Core/Inc/modbus.h
+++ Core/Inc/modbus.h
@@ -1,25 +1,38 @@
-#include "stm32f4xx_hal.h"
 #include <stdbool.h>
+#include "stm32f4xx_hal.h"
 
 #ifndef MODBUS_H_
 #define MODBUS_H_
 
-// Adjustable parameters
+/* ADJUSTABLE PARAMETERS */
 #define DEVICE_ID 0x02
 #define REGISTERS_NUMBER 16
 
-#define REQUEST_TYPE_READ 0
-#define REQUEST_TYPE_WRITE 1
-
 typedef int16_t modbus_register;
 
-/* Set value in Modbus register */
+/**
+ * Sets value in Modbus register
+ * @param offset Number representing offset of given register (for 40001 use 0)
+ * @param value Value to set in given register
+ * @return True if success, false otherwise
+ */
 bool modbus_set_reg_value(uint16_t offset, int16_t value);
 
-/* Get value from Modbus register */
+/**
+ * Get value from Modbus register
+ * @param offset Number representing offset of given register (for 40001 use 0)
+ * @return Value of given register
+ */
 int16_t modbus_get_reg_value(uint16_t offset);
 
-/* Process Modbus frame and generate response */
-bool modbus_process_frame(uint8_t * frame, uint16_t frame_size, uint8_t * response_buffer, uint16_t * response_length);
+/**
+ * Process Modbus frame and get response
+ * @param request Pointer to byte buffer that keeps frame to process
+ * @param request_size Size of frame to process (in bytes)
+ * @param response Pointer to buffer where Modbus response will be written
+ * @param response_size Pointer to variable where size of Modbus response (in bytes) will be written
+ * @return Value of given register
+ */
+bool modbus_process_frame(uint8_t * request, uint16_t request_size, uint8_t * response, uint16_t * response_size);
 
 #endif /* MODBUS_H_ */
