f217edb2d4b9a390d4fec879993aae70a4877a24
Grzegorz Cholewa <grzesiekcholewa@gmail.com>
2021-02-12 20:43:52 +0100

    Improved logger module

--- Core/Inc/logger.h
+++ Core/Inc/logger.h
@@ -1,2 +1,10 @@
 
-void log_usb(char * buffer);
+enum log_level_type {LEVEL_NONE = 0, LEVEL_ERROR = 1, LEVEL_INFO = 2, LEVEL_DEBUG = 3};
+
+void logger_set_level(int8_t level_to_set);
+
+void log_error(char * string_buffer);
+
+void log_info(char * string_buffer);
+
+void log_debug(char * string_buffer);
--- Core/Src/logger.c
+++ Core/Src/logger.c
@@ -2,9 +2,49 @@
 #include <logger.h>
 #include <string.h>
 
+/* PRIVATE METHOD */
+void log_usb(char * string_buffer);
+
+enum log_level_type level;
+
+void logger_set_level(int8_t level_to_set)
+{
+	level = level_to_set;
+}
+
+uint8_t get_level()
+{
+	return (uint8_t)level;
+}
+
+void log_error(char * string_buffer)
+{
+	if (get_level() >= LEVEL_ERROR)
+	{
+		log_usb(string_buffer);
+	}
+}
+
+void log_info(char * string_buffer)
+{
+	if (get_level() >= LEVEL_INFO)
+	{
+		log_usb(string_buffer);
+	}
+}
+
+void log_debug(char * string_buffer)
+{
+	if (get_level() == LEVEL_DEBUG)
+	{
+		log_usb(string_buffer);
+	}
+}
+
 void log_usb(char * string_buffer)
 {
 	const uint16_t max_string_size = 100;
+	const int max_retries = 1000;
 	const uint8_t line_ending[] = {'\n', '\r'};
 
 	uint8_t string_size = strlen(string_buffer);
@@ -18,5 +58,11 @@ void log_usb(char * string_buffer)
 	memcpy(output_buffer, string_buffer, string_size);
 	memcpy(output_buffer+string_size, line_ending, 2);
 
-	CDC_Transmit_FS((uint8_t*)output_buffer, sizeof(output_buffer));
+	int result = 1;
+	int retries = 0;
+	while (result != 0 && retries < max_retries)
+	{
+		result = CDC_Transmit_FS((uint8_t*)output_buffer, sizeof(output_buffer));
+		retries++;
+	}
 }
